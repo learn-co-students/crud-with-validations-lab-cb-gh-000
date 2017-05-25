d2d97964c6ca2956784d55d40bd5702966e05784
Robert Wayne <robertjwayne@gmail.com>
2017-05-25 12:52:00 -0400

    Add validation for release year less than or equal to current year

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
-  validates :release_year, presence: true, if: :released?
+  validates :release_year, presence: true, numericality: { less_than_or_equal_to: Time.now.year }, if: :released?
 end
