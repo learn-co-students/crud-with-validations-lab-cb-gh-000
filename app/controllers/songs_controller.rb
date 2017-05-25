dde6f96311e9c87121811d0e90bf5733d0f5bb8d
Robert Wayne <robertjwayne@gmail.com>
2017-05-25 12:23:50 -0400

    Add new and create actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,20 @@
 class SongsController < ApplicationController
+  def create
+    @song = Song.new(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
   def edit
     @song = Song.find(params[:id])
   end
 
   def index
     @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
   end
 
   def show
