8e36365cba3956f97800eb64a88208ce1b41a366
Quinn Dizon <qdizon@gmail.com>
2018-08-31 13:06:51 +0000

    working

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {
-      less_than_or_equal_to: Date.today.year
+      less_than_or_equal_to: Date.current.year
     }
   end
 
